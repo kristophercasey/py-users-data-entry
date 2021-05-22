fe5186a696482fce4085b48980a6acfb3322e272
SGarGu90 <sgargu90@gmail.com>
2021-05-22 21:03:42 +0200

    Instantiate personManager on orchestator

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,3 +1,7 @@
+from src.PersonManager.PersonManager import PersonManager
+
+INSERT_DATA_OPT = 1
+EXIT_OPT = 0
 
 def orchestrator(config, logger):
     """Orchestrator app
@@ -5,5 +9,9 @@ def orchestrator(config, logger):
 
     try:
         logger.log.info("App orchestrator execution\n")
+        logger.log.info("App orchestrator arguments {0} \n".format(config))
+
+        personManager = PersonManager(config = config, logger = logger)
+
     except Exception as err:
         logger.log_traceback(err)
